f0c5d70d07a2cd967f95974954e003d709570e50
Blesson V <blesson.connect@gmail.com>
2026-06-29 23:26:26 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// DB configs not avalilabe 
+//Loggers missing 
+
 @SpringBootApplication
 public class DemoApplication {
 
